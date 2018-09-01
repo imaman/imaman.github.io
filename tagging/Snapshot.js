3f9281cef58de4cb972c5ce757c039a3225d2da3
Itay Maman <itay.maman@gmail.com>
2018-09-01 09:00:58 +0300

    delete files

--- tagging/Snapshot.js
+++
@@ -1,7 +0,0 @@
-class Snapshot {
-    constructor(savedDom, imageUrl, metadata) {
-        Object.defineProperty(this, "savedDom", {value: savedDom, enumerable: true});
-        Object.defineProperty(this, "imageUrl", {value: imageUrl, enumerable: true});
-        Object.defineProperty(this, "metadata", {value: metadata, enumerable: true});
-    }
-}
